d9890d46e32907a1882f4578c998900823722465
Maryna Kyrychynska <m_kirichok@ukr.net>
2024-02-05 12:17:25 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const population = document.querySelectorAll(".population");
+const populationNumbers = [
+    ...population
+].map((item)=>+item.innerText.replaceAll(",", ""));
+const total = populationNumbers.reduce((a, b)=>a + b, 0);
+const average = total / populationNumbers.length;
+const totalPopulation = document.querySelector(".total-population");
+const averagePopulation = document.querySelector(".average-population");
+totalPopulation.textContent = `${total.toLocaleString("en-US")}`;
+averagePopulation.textContent = `${average.toLocaleString("en-US")}`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
